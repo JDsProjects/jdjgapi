2e44a21cfdbc6eb8354297fd0bdb839693b6c889
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-11 03:35:21 +0000

    added a license.

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask
+import http.server
+import socketserver
+import json
+import math
 
-app = Flask("JD-api")
+port = 3000
 
-@app.route("/")
-def main():
-  return "Welcome to the JDJG Inc. Official api :D"
+class handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        if self.path == '/':
+            file = open('static/index.html','rb')
+            data = file.read()
+            file.close()
+        elif self.path == '/api/objection':
+            file = open('static/data.json','r')
+            data_json = file.read()
+            file.close()
+            data_dict = json.loads(data_json)
+            data_array = data_dict["objection"]
+            data = data_array[Math.floor(Math.random()*items.length)]
+        else:
+            file = open('static/404.html','rb')
+            data = file.read()
+            file.close()
 
-app.run(host='0.0.0.0', port=3000,threaded=True)
+        self.wfile.write(data)
+
+with socketserver.TCPServer(("", port), handler) as server:
+    server.serve_forever()
