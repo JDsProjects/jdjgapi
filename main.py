7b9f674f2974995e93e42552f544ded428072142
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-28 15:34:45 +0000

    updated keys.

--- main.py
+++ main.py
@@ -7,28 +7,39 @@ app = Flask(__name__)
 @app.route('/')
 def handleRoot():
     with open('index.html', 'r') as file:
-        data = file.read()
+        data = file.read(), 200, {'content-type':'text/html'}
     return data
+
+@app.route('/api/')
+def handleApi():
+    with open('api.html', 'r') as file:
+        data = file.read(), 200, {'content-type':'text/html'}
+    return data
+
 
 @app.route('/api/<endpoint>')
 def handleEndpoint(endpoint):
-  if endpoint == "":
-    with open('api.html', 'r') as file:
-        data = file.read()
-  else:
-    with open('data.json', 'r') as file:
-        data_json = json.loads(file.read())
-    try:
-        data_array = data_json[endpoint]
-    except KeyError as e:
-        print(e)
-    data = str.encode(random.choice(data_array))
+  with open('data.json', 'r') as file:
+    dataJson = json.loads(file.read())
+  try:
+      dataArray = dataJson[endpoint]
+      tempData = str(random.choice(dataArray))
+      if endpoint == "objection" or endpoint == "opinional":
+        tempDict = {"url": tempData}
+      else:
+        tempDict = {"text": tempData}
+      data = json.dumps(tempDict), 200, {'content-type':'application/json'}
+        
+  except KeyError as e:
+    print(e)
+    errorDict={"error":f"{endpoint} isn't a valid endpoint"}
+    data = json.dumps(errorDict), 404, {'content-type':'application/json'}
   return data
 
 @app.errorhandler(404)
 def handle404(error):
     with open('404.html', 'r') as file:
         data = file.read()
-    return data
+    return data, 404, {'content-type':'text/html'}
 
 app.run(host='0.0.0.0', port=3000)
