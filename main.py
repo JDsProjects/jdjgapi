f7f02d3d9dc8aa011bb9d693cc595c65f62d7e10
JDJG <ghostzap123@gmail.com>
2024-03-02 11:56:48 -0500

    Looks a lot better.

--- main.py
+++ main.py
@@ -1,46 +1,65 @@
 from fastapi import FastAPI
-import json, random
+import random
+import json
+
+data = json.loads(file.read())
 
 app = FastAPI()
 
-@app.get('/')
-async def handleRoot():
-    with open('index.html', 'r') as file:
-        data = file.read(), 200, {'content-type':'text/html'}
-    return data
+@app.get("/")
+async def root():
+    return {"message" : "welcome to jdjg api"}
 
-@app.get('/api/')
-async def handleApi():
-    with open('api.html', 'r') as file:
-        data = file.read(), 200, {'content-type':'text/html'}
-    return data
+@app.get("/api/")
+async def api():
+    return {"endpoints" : "wip"}
 
+@app.get("/api/objection")
+async def objection():
+    text = data["objection"]
 
-@app.get('/api/<endpoint>')
-async def handleEndpoint(endpoint):
-  with open('data.json', 'r') as file:
-    dataJson = json.loads(file.read())
-  try:
-      dataArray = dataJson[endpoint]
-      tempData = str(random.choice(dataArray))
-      if endpoint == "objection" or endpoint == "opinional":
-        tempDict = {"url": tempData}
-      else:
-        tempDict = {"text": tempData}
-      data = json.dumps(tempDict), 200, {'content-type':'application/json'}
-        
-  except KeyError as e:
-    print(e)
-    errorDict={"error":f"{endpoint} isn't a valid endpoint"}
-    data = json.dumps(errorDict), 404, {'content-type':'application/json'}
-  return data
+    return {"url" : random.choice(text)}
 
-# hi
-@app.errorhandler(404)
-async def handle404(error):
-    with open('404.html', 'r') as file:
-        data = file.read()
-    return data, 404, {'content-type':'text/html'}
+    # not sure how to return it
+
+@app.get("/api/advice/")
+async def advice():
+    text = data["objection"]
+
+    return {"text" : random.choice(text)}
+
+@app.get("/api/noslur/")
+async def noslur():
+    text = data["noslur"]
+
+    {"text" : random.choice(text)}
+
+@app.get("/api/random-message/")
+async def random_message():
+    text = data["randomMessage"]
+
+    return {"text" : random.choice(text)}
+
+@app.get("/api/insult/")
+async def insult():
+    text = data["insult"]
+
+    return {"text" : random.choice(text)}
+
+@app.get("/api/compliment/")
+async def compliment():
+    text = data["compliment"]
+
+    return {"text" : random.choice(text)}
+
+@app.get("/api/opinional/")
+async def opinional():
+    text = data["opinional"]
+
+    return {"url" : random.choice(text)}
+
+# handle 404 errors.
+
 
 app.run(host = '0.0.0.0', port=3000)
-#check out the misc thing in Senarc, this will not be archived for now, but please don't attempt to make a pull request, Thanks.
+
