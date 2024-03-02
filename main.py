9d8aef72e749b17603c16e216007342e2ad938ae
JDJG <ghostzap123@gmail.com>
2024-03-02 23:17:09 +0000

    fixed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from gtts import gTTS
 
-data = json.loads(file.read("jdjg_data.json"))
+data = json.loads("jdjg_data.json")
 
 app = FastAPI()
 
